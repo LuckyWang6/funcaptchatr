e97a47c53dfe1ba41607f8f8a2b3820d96ee0221
w1387891 <132724081+w1387891@users.noreply.github.com>
2024-06-28 11:48:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from models.siamese import Siamese
 from custom_dataset import CustomSiameseDataset
 
 # 配置参数
-batch_size = 256
+batch_size = 200
 image_folder = './data'
 validation_split = 0.2  # 验证集比例
 num_epochs = 300
